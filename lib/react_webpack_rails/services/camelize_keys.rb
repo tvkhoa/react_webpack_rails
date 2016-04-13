25e0b741fff6ac926c779f334bd32a9fb93df739
Marek Mikołajczyk <marek.mikolajczyk@netguru.pl>
2016-04-13 16:38:43 +0200

    Fixing camelize keys

--- lib/react_webpack_rails/services/camelize_keys.rb
+++ lib/react_webpack_rails/services/camelize_keys.rb
@@ -1,11 +1,25 @@
 module ReactWebpackRails
   module Services
     class CamelizeKeys
-      def self.call(props)
+      def call(props)
         return props unless props.is_a?(Hash)
         props.inject({}) do |h, (k, v)|
-          h[k.to_s.camelize(:lower)] = v.is_a?(Hash) ? self.call(v) : v
+          h[k.to_s.camelize(:lower)] = prop_value(v)
           h
+        end
+      end
+
+      def self.call(props)
+        new.call(props)
+      end
+
+      private
+
+      def prop_value(prop_value)
+        case prop_value.class.name
+        when 'Array' then prop_value.map { |prop| call(prop) }
+        when 'Hash' then call(prop_value)
+        else prop_value
         end
       end
     end
