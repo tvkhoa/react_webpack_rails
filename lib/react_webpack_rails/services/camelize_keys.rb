0c6a2ed60838f74f299f96811d0ea27d83eff851
Rafał Gawlik <rafal.maly@gmail.com>
2016-04-15 11:39:20 +0200

    accept top level array

--- lib/react_webpack_rails/services/camelize_keys.rb
+++ lib/react_webpack_rails/services/camelize_keys.rb
@@ -1,11 +1,11 @@
 module ReactWebpackRails
   module Services
     class CamelizeKeys
-      def call(props)
-        return props unless props.is_a?(Hash)
-        props.inject({}) do |h, (k, v)|
-          h[k.to_s.camelize(:lower)] = prop_value(v)
-          h
+      def call(data)
+        case data.class.name
+        when 'Array' then data.map { |element| call(element) }
+        when 'Hash' then camelize_hash(data)
+        else data
         end
       end
 
@@ -15,11 +15,10 @@ module ReactWebpackRails
 
       private
 
-      def prop_value(prop_value)
-        case prop_value.class.name
-        when 'Array' then prop_value.map { |prop| call(prop) }
-        when 'Hash' then call(prop_value)
-        else prop_value
+      def camelize_hash(data)
+        data.inject({}) do |hash, (key, value)|
+          hash[key.to_s.camelize(:lower)] = call(value)
+          hash
         end
       end
     end
