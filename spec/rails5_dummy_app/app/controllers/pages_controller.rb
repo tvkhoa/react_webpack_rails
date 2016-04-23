aff5befc72f4417c63f63da8cd2dfe951fbd4d5e
Rafał Gawlik <rafal.maly@gmail.com>
2016-04-23 23:01:39 +0200

    create rails5_dummy_app

---
+++ spec/rails5_dummy_app/app/controllers/pages_controller.rb
@@ -0,0 +1,12 @@
+class PagesController < ApplicationController
+  def client_side_hello_world
+    @name = 'username'
+  end
+
+  def react_component_renderer_test
+    render react_component: 'HelloWorld', props: { name: 'render component test' }
+  end
+
+  def home
+  end
+end
