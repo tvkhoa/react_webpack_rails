1101045dd60048692cb85735d35b17a22d6624a6
Rafał Gawlik <rafal.maly@gmail.com>
2016-04-25 01:02:39 +0200

    use rwr_element instead of react_element

--- lib/react_webpack_rails/view_helpers.rb
+++ lib/react_webpack_rails/view_helpers.rb
@@ -25,17 +25,17 @@ module ReactWebpackRails
       props = Services::CamelizeKeys.call(props) if Rails.application.config.react.camelize_props
       if server_side(options.delete(:server_side))
         result = NodeIntegrationRunner.new('react-component', props: props, name: name).run
-        react_element('react-component', { props: props, name: name }, options) do
+        rwr_element('react-component', { props: props, name: name }, options) do
           result.html_safe
         end
       else
-        react_element('react-component', { props: props, name: name }, options)
+        rwr_element('react-component', { props: props, name: name }, options)
       end
     end
 
     def react_router(name)
       deprecation_warning
-      react_elemen('react-router', name: name)
+      rwr_element('react-router', name: name)
     end
 
     private
